528a35ba6f7173469ab020340d9920d26bb15b7b
tgorsk15 <tgbusiness463@gmail.com>
2025-01-23 20:10:20 -0600

    build testFetch to ensure app can connect to backend and 3rd party API

--- src/Pages/NbaPage/NbaPage.jsx
+++ src/Pages/NbaPage/NbaPage.jsx
@@ -1,11 +1,34 @@
-
+import { getEnvVariable } from "../../utils/apiSetter"
 
 export const NbaPage = () => {
 
+    async function testFetch() {
+        const options = {
+            method: "GET",
+            headers: {"Content-Type": "application/json"},
+        }
+
+        try {
+            const apiUrl = getEnvVariable()
+            const response = await fetch(`${apiUrl}`, options) 
+            const data = await response.json()
+            console.log(data)
+
+        } catch(err) {
+            console.log(err)
+        }
+        
+    }
+    // testFetch()
 
     return (
         <div>
             <h1>here is the nbaPage</h1>
+            <button
+                onClick={testFetch}
+            >
+                Test
+            </button>
         </div>
     )
 }

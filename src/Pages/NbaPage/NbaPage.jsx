bc9d1b2a998258e5d432331b070913fdb8cd1677
tgorsk15 <tgbusiness463@gmail.com>
2025-01-25 14:10:54 -0600

    Begin setting up React Query

--- src/Pages/NbaPage/NbaPage.jsx
+++ src/Pages/NbaPage/NbaPage.jsx
@@ -1,25 +1,45 @@
+import { useState } from "react"
+import { useQuery, QueryClient, useQueryClient } from "@tanstack/react-query"
 import { getEnvVariable } from "../../utils/apiSetter"
 
+const queryClient = new QueryClient()
+
 export const NbaPage = () => {
+    const [teamData, changeData] = useState(true) 
+
+    const options = {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+    }
+
+    const queryClient = useQueryClient
+    const {data: teams, isPending} = useQuery({
+        queryFn: async () => {
+            const apiUrl = getEnvVariable()
+            const response = await fetch(`${apiUrl}`, options)
+            return await response.json()
+        },
+        queryKey: 'teams'
+    })
 
     async function testFetch() {
-        const options = {
-            method: "GET",
-            headers: {"Content-Type": "application/json"},
-        }
+        // const options = {
+        //     method: "GET",
+        //     headers: {"Content-Type": "application/json"},
+        // }
 
-        try {
-            const apiUrl = getEnvVariable()
-            const response = await fetch(`${apiUrl}`, options) 
-            const data = await response.json()
-            console.log(data)
+        // try {
+        //     const apiUrl = getEnvVariable()
+        //     const response = await fetch(`${apiUrl}`, options) 
+        //     const data = await response.json()
+        //     console.log(data)
 
-        } catch(err) {
-            console.log(err)
-        }
-        
+        // } catch(err) {
+        //     console.log(err)
+        // }
+
     }
-    // testFetch()
+
 
     return (
         <div>
@@ -27,8 +47,17 @@ export const NbaPage = () => {
             <button
                 onClick={testFetch}
             >
-                Test
+                Get teams
             </button>
+
+            {isPending && (
+                <div>Its Loading ...</div>
+            )}
+
+            {data && (
+                <h2>teams is loaded</h2>
+            )}
+
         </div>
     )
 }

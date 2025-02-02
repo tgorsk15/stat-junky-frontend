67706573eedb2cf1be1691c635b0a463afe28de4
tgorsk15 <tgbusiness463@gmail.com>
2025-02-02 13:30:30 -0600

    Revamp playerCard loading state to ensure correct loading is shown

--- src/Components/PlayerCard/PlayerCard.jsx
+++ src/Components/PlayerCard/PlayerCard.jsx
@@ -8,17 +8,18 @@ export const PlayerCard = ({isPlayer2}) => {
     console.log('boolean player check:', isPlayer2)
 
     const { player1Load, player1Stats, player2Load, player2Stats } = usePlayer()
-    let currentPlayerStats = player1Stats;
+    const currentPlayerStats = isPlayer2 ? player2Stats : player1Stats
+    const isLoading = isPlayer2 ? player2Load : player1Load
 
-    // ** below checks to see if is player2
-    if (isPlayer2) {
-        currentPlayerStats = player2Stats
+    if (isLoading) {
+        return (
+            <h2>Loading {isPlayer2 ? 'player 2' : 'player 1'}</h2>
+        )
     }
 
     if (!currentPlayerStats) {
         return
     }
-
     
 
     const player = currentPlayerStats.playerData
@@ -28,69 +29,63 @@ export const PlayerCard = ({isPlayer2}) => {
 
     return (
         <>
-            {/* loading not working... need to figure out */}
-            {player1Load ? (
-                 <h2>Loading</h2>
-
-            ) : (
                 
-                currentPlayerStats && (
-                    <div className={cardStyles.cardContainer}>
-                        <h2 className={cardStyles.playerTitle}>
-                            {player.first_name} {player.last_name}
-                        </h2>
+            {currentPlayerStats && (
+                <div className={cardStyles.cardContainer}>
+                    <h2 className={cardStyles.playerTitle}>
+                        {player.first_name} {player.last_name}
+                    </h2>
 
-                        <div className={cardStyles.cardBody}>
-                            <table className={cardStyles.dataTable}>
-                                <thead>
-                                   <tr className={cardStyles.headerRow}>
-                                        <th>Season</th>
-                                        <th>GS</th>
-                                        <th>PTS</th>
-                                        <th>RBS</th>
-                                        <th>ORB</th>
-                                        <th>AST</th>
-                                        <th>STL</th>
-                                        <th>BLK</th>
-                                        <th>TOV</th>
-                                        <th>FG%</th>
-                                        <th>FGM</th>
-                                        <th>3P%</th>
-                                        <th>3PM</th>
-                                        <th>FTM</th>
-                                        <th>FT%</th>
-                                    </tr> 
-                                </thead>
-                                
-                                <tbody>
-                                   {seasons.map((year) => {
-                                        return (
-                                            <tr key={year.season} className={cardStyles.seasonRow}>
-                                                <td>{year[0].season}</td>
-                                                <td>{year[0].games_played}</td>
-                                                <td>{Math.round(year[0].pts * 10) / 10}</td>
-                                                <td>{Math.round(year[0].reb * 10) / 10}</td>
-                                                <td>{Math.round(year[0].oreb * 10) / 10}</td>
-                                                <td>{Math.round(year[0].ast * 10) / 10}</td>
-                                                <td>{Math.round(year[0].stl * 10) / 10}</td>
-                                                <td>{Math.round(year[0].blk * 10) / 10}</td>
-                                                <td>{Math.round(year[0].turnover * 10) / 10}</td>
-                                                <td>{Math.round(year[0].fg_pct * 10) / 10}</td>
-                                                <td>{Math.round(year[0].fgm * 10) / 10}</td>
-                                                <td>{Math.round(year[0].fg3_pct * 10) / 10}</td>
-                                                <td>{Math.round(year[0].fg3m * 10) / 10}</td>
-                                                <td>{Math.round(year[0].ftm * 10) / 10}</td>
-                                                <td>{Math.round(year[0].ft_pct * 10) / 10}</td>
-                                            </tr>
-                                        )
-                                    })} 
-                                </tbody>
-                                
-                            </table>
+                    <div className={cardStyles.cardBody}>
+                        <table className={cardStyles.dataTable}>
+                            <thead>
+                                <tr className={cardStyles.headerRow}>
+                                    <th>Season</th>
+                                    <th>GS</th>
+                                    <th>PTS</th>
+                                    <th>RBS</th>
+                                    <th>ORB</th>
+                                    <th>AST</th>
+                                    <th>STL</th>
+                                    <th>BLK</th>
+                                    <th>TOV</th>
+                                    <th>FG%</th>
+                                    <th>FGM</th>
+                                    <th>3P%</th>
+                                    <th>3PM</th>
+                                    <th>FTM</th>
+                                    <th>FT%</th>
+                                </tr> 
+                            </thead>
                             
-                        </div>
-                    </div> 
-                )
+                            <tbody>
+                                {seasons.map((year) => {
+                                    return (
+                                        <tr key={year.season} className={cardStyles.seasonRow}>
+                                            <td>{year[0].season}</td>
+                                            <td>{year[0].games_played}</td>
+                                            <td>{Math.round(year[0].pts * 10) / 10}</td>
+                                            <td>{Math.round(year[0].reb * 10) / 10}</td>
+                                            <td>{Math.round(year[0].oreb * 10) / 10}</td>
+                                            <td>{Math.round(year[0].ast * 10) / 10}</td>
+                                            <td>{Math.round(year[0].stl * 10) / 10}</td>
+                                            <td>{Math.round(year[0].blk * 10) / 10}</td>
+                                            <td>{Math.round(year[0].turnover * 10) / 10}</td>
+                                            <td>{Math.round(year[0].fg_pct * 10) / 10}</td>
+                                            <td>{Math.round(year[0].fgm * 10) / 10}</td>
+                                            <td>{Math.round(year[0].fg3_pct * 10) / 10}</td>
+                                            <td>{Math.round(year[0].fg3m * 10) / 10}</td>
+                                            <td>{Math.round(year[0].ftm * 10) / 10}</td>
+                                            <td>{Math.round(year[0].ft_pct * 10) / 10}</td>
+                                        </tr>
+                                    )
+                                })} 
+                            </tbody>
+                            
+                        </table>
+                        
+                    </div>
+                </div> 
             )}
             
         </>
@@ -100,7 +95,5 @@ export const PlayerCard = ({isPlayer2}) => {
 }
 
 PlayerCard.propTypes = {
-    // playerStats: PropTypes.any,
-    // playerLoad: PropTypes.bool,
     isPlayer2: PropTypes.bool
 }

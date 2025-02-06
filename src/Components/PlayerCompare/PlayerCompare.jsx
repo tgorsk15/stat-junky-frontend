b3977f138bab763d2f756dc4ab8026a26f731f4e
tgorsk15 <tgbusiness463@gmail.com>
2025-02-05 18:14:00 -0600

    Fix bug with playerResults list

--- src/Components/PlayerCompare/PlayerCompare.jsx
+++ src/Components/PlayerCompare/PlayerCompare.jsx
@@ -3,4 +3,7 @@ export const PlayerCompare = () => {
     return (
         <div>this is compare</div>
     )
+    // TMW 2/5:
+    // start cretaing this Component, draw out what I want table to look like
+    // ...see if state structure needs to be changed
 }

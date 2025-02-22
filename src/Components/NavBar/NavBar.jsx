786354e144ac0fd0f6fae89739787b18ea287084
tgorsk15 <tgbusiness463@gmail.com>
2025-02-22 12:55:06 -0600

    Give functionality to the dropdown and making it dissapear on off-click

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import '../../App.css'
@@ -6,12 +6,31 @@ import '../../App.css'
 export const NavBar = ({currentLeague, changeLeague}) => {
     const [isDropdownOpen, setDropdown] = useState(false)
 
-    function handleLeagueClick(e) {
-        if (!isDropdownOpen) {
-            setDropdown(true)
-        } else {
-            setDropdown(false)
+    const dropdownRef = useRef(null)
+    const downCaretRef = useRef(null)
+
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if (!dropdownRef.current.contains(e.target) && 
+            !downCaretRef.current.contains(e.target)) {
+                setDropdown(false)
+                console.log(dropdownRef.current)
+            } else {
+                setDropdown(true)
+            }
+            
         }
+
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick)
+        }
+    }, [])
+
+
+    function handleLeagueClick(e) {
+        setDropdown(true)
     }
 
     return (
@@ -20,14 +39,16 @@ export const NavBar = ({currentLeague, changeLeague}) => {
                 <h2>Stat Junky</h2>
                 <div className='linksContainer'>
                     <div className='leagueSelection'>
-                        <button className='leagueBtn'onClick={handleLeagueClick}>
+                        <button className='leagueBtn'onClick={handleLeagueClick} ref={downCaretRef}>
                             {currentLeague}  
                             <FontAwesomeIcon icon={faAngleDown} className='downCaretIcon'/>
                         </button>
                         
                         <div 
+                            ref={dropdownRef}
                             className={`leagueDropdown ${isDropdownOpen && ('dropdownVisible')}`}
                         >
+                            {/* in the future, clicking the below will trigger changeLeague... */}
                             <p>NBA</p>
                             <p>NFL</p>
                             <p>NHL</p>

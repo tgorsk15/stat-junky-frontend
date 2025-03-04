157bb3986c977f155a082690ab83783c570f0fc7
tgorsk15 <tgbusiness463@gmail.com>
2025-03-04 12:59:38 -0600

    Add google analytics script tags

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      'Content-Security-Policy': "default-src 'self'; script-src 'self' https://cloud.umami.is; connect-src 'self' https://cloud.umami.is"
-    }
-  }
+
 })

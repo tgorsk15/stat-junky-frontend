365120de23e202df6682415d95c5da81ae1e5290
tgorsk15 <tgbusiness463@gmail.com>
2025-03-04 10:31:30 -0600

    Remove _headers, add config back to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; script-src 'self' https://cloud.umami.is; connect-src 'self' https://cloud.umami.is"
+    }
+  }
 })
